ec4c6e247ac84ecb7e4c8754d9c925039ec0ee6c
samakintayo <tayoakinnayajo@gmailcom>
2022-11-22 22:38:49 +0000

    fix handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -9,11 +9,10 @@
  * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
- *
  * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-	int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
@@ -26,7 +25,6 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
-
 	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
--- main.h
+++ main.h
@@ -42,6 +42,7 @@ struct fmt
  */
 
 typedef struct fmt fmt_t;
+
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
