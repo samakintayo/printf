6f1b3e07284fad029d3433529604a0ba9ed0d01a
samakintayo <tayoakinnayajo@gmailcom>
2022-11-22 20:05:44 +0000

    fixed _printf.c error ; was missing

--- _printf.c
+++ _printf.c
@@ -43,7 +43,7 @@ int _printf(const char *format, ...)
 		{
 			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
-			width = get_width(format, &i, list)
+			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
